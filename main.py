32bba51d6ba882d0f8ede62e47f7f795a5a07fd1
RomeandAlphaGuylol <messidtz345@gmail.com>
2023-05-26 18:45:16 +0530

    Set Biolerplate

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
 from createName import *
 from getName import *
+from sanboh import *
 print("SanBoh!")
 
 usernamefile = open("username.txt", "r").read()
 if usernamefile==[]:
     name()
 else:
+    sanboh.main()
--- sanboh.py
+++ sanboh.py
@@ -3,3 +3,12 @@ from createName import *
 from getName import *
 
 # Code will be implemented here (Chat Part)
+class sanboh:
+    def main():
+        chat = input("Chat with SanBo! -> ")
+        if chat==[]:
+            pass
+        elif chat==[]:
+            pass
+        else:
+            pass
