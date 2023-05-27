fa83f68cbf4c1c141796138d0fb79098cc874a56
RomeandAlphaGuylol <lunaruser9@gmail.com>
2023-05-27 17:33:37 +0530

    b

--- sanboh.py
+++ sanboh.py
@@ -11,12 +11,18 @@ class sanboh:
         else:
             print("SanBoh!")
         while True:
-            
             chat = input("Chat with SanBo! -> ")
+            if chat.startswith("hi"):
+                response_greeting = ["hello there :> Im SanBo! an Idiotic AI!!!", "Hi!", "Hi, Im SanBo an Idiotic AI :D"]
+                print(random.choice(response_greeting))
             if chat.startswith("hello"):
                 response_greeting = ["hello there :> Im SanBo! an Idiotic AI!!!", "Hi!", "Hi, Im SanBo an Idiotic AI :D"]
                 print(random.choice(response_greeting))
             elif chat=="exit":
                 quit()
+            elif chat.__contains__("my name"):
+                openUserFile = open("username.txt", "r").read()
+                lines = openUserFile.strip()
+                print(lines)
             else:
                 print("As I said! Im an idiot and I dont seem to know that answer :<")
